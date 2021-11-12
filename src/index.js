be231b97ebcd262c88a8674e43cf8212f3284f10
Team LevX <team@levx.io>
2021-11-12 12:53:48 -0500

    Fix

--- src/index.js
+++ src/index.js
@@ -19,15 +19,15 @@ client.on("message", async (msg) => {
         if (role) {
           await msg.member.roles.add(role);
           await msg.channel.send(
-            "> **<@" +
+            "> <@" +
               userId +
               ">, you are now a <@&" +
               I_DO_AS_THE_DICTATOR_GUIDES_ROLE_ID +
-              ">!**"
+              ">!"
           );
         } else {
           await msg.channel.send(
-            "> **<@" + userId + ">, cannot add you to the role now.**"
+            "> <@" + userId + ">, cannot add you to the role now."
           );
         }
       } else {
