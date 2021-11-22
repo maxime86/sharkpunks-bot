0c00ab16ba1df2068cb038414f243cd67eaf56c9
Team LevX <team@levx.io>
2021-11-21 21:22:07 -0800

    Update

--- src/index.js
+++ src/index.js
@@ -12,8 +12,7 @@ client.on("message", async (msg) => {
   if (msg.content === "!brainwash-me") {
     await brainwashMe();
   } else if (msg.content.toLowerCase().match(/wh?en/)) {
-    await msg.channel.send("idk", {
-      reply: msg,
+    await msg.reply("idk", {
       files: [
         "https://cdn.discordapp.com/attachments/911405391747883019/911670069124268072/unknown.gif",
       ],
